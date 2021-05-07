7a7d5170de0b7e857c4d56bb962a20b48cddb04d
Marilius <38788888+Marilius@users.noreply.github.com>
2021-05-07 23:17:10 +0300

    Update task.c

--- task.c
+++ task.c
@@ -103,8 +103,7 @@ int main(int argc, char **argv)
 {
   //ps по условию x > 0
   int keys[2] = {0, 0};
-  for(int i = 1; i < argc; i++)
-    {
+  int i = 1;
         if(strcmp("-help", argv[i]) == 0)// help option
         {
             printf("-help\tdescribe all options\n-iters\tprint num of iterations\n-roots\tprint roots\n-test args\ttest this program\n\
@@ -168,7 +167,6 @@ to run on prepared test args must be: -test prepared n\n n - index of test\n");
             	return 0;
             }
         }
-    }
     else
     {
         double r1 = root(f1, f2, -3, 7, 0.001);//first root
